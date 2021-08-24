423b6ae284b6ba641776edacb594265524cc6ae1
ismaeelhaider72 <66254080+ismaeelhaider72@users.noreply.github.com>
2021-08-24 11:37:04 +0500

    Update lambda.tf

--- APIGATWAY_LAMBDA_assignmentWithModules/modules/lambdas/lambda.tf
+++ APIGATWAY_LAMBDA_assignmentWithModules/modules/lambdas/lambda.tf
@@ -122,46 +122,5 @@ resource "aws_lambda_function" "update_lambda" {
 
 
 
-//////////////////
-
-output "create_function_name2" {
-  value = "${aws_lambda_function.create_lambda.function_name}"
-}
-
-output "update_function_name2" {
-  value = "${aws_lambda_function.update_lambda.function_name}"
-}
-
-
-output "delete_function_name2" {
-  value = "${aws_lambda_function.delete_lambda.function_name}"
-}
-
-output "get_function_name2" {
-  value = "${aws_lambda_function.get_lambda.function_name}"
-}
-
-
-
-
-////////////////////////////////// invoking arn ////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////
-
-output "create_arn" {
-  value = "${aws_lambda_function.create_lambda.invoke_arn}"
-}
-
-output "update_arn" {
-  value = "${aws_lambda_function.update_lambda.invoke_arn}"
-}
-
-output "delete_arn" {
-  value = "${aws_lambda_function.delete_lambda.invoke_arn}"
-}
-
-
-output "get_arn" {
-  value = "${aws_lambda_function.get_lambda.invoke_arn}"
-}
 
 
